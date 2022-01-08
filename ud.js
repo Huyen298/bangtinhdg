4412477cc0aff3aef3ba41249127f7215eee6ccf
unknown <hn012041211998@gmail.com>
2022-01-08 15:51:22 +0700

    btinhdongian

---
+++ ud.js
@@ -0,0 +1,7 @@
+function tinhtoan(){
+    let number1 = Number(document.getElementById("number1").value;)
+    let number2 = Number(document.getElementById("number2").value;)
+    let tong = number1+number2;
+    document.getElementById("result").innerText =tong;
+
+}
